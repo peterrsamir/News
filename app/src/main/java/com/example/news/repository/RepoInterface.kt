e46bf4bd555a1df5b710f5a93b7f794543e06ed5
mina wael <menawael831@gmail.com>
2022-03-26 14:52:58 +0200

    modify network stata

--- app/src/main/java/com/example/news/repository/RepoInterface.kt
+++ app/src/main/java/com/example/news/repository/RepoInterface.kt
@@ -1,8 +1,6 @@
 package com.example.news.repository
 
-import com.example.example.News
 import com.example.news.network.NewsState
-import retrofit2.Response
 
 interface RepoInterface {
     suspend fun getApiData(): NewsState
